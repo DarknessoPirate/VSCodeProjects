a6aeac483fe47518605d6763bb945156ee7ec115
Dawid Pacia <dp307910@student.polsl.pl>
2023-11-25 01:18:16 +0100

    works

--- Python/Lista/Zad58.py
+++ Python/Lista/Zad58.py
@@ -1,5 +1,22 @@
-import math
 
-print("test")
+letterDict = {'I':1,
+           'V':5,
+           'X':10,
+           'L':50,
+           'C':100,
+           'D':500,
+           'M':1000}
 
-print("TEST2")
+def fromRoman(str):
+    sum = 0
+
+    for i in range(len(str)-1,-1,-1):
+        if(i+1 >= len(str)):
+            sum += letterDict[str[i]]
+        elif(letterDict[str[i]] >= letterDict[str[i+1]]):
+            sum += letterDict[str[i]]
+        else:
+            sum -= letterDict[str[i]]
+
+    return sum
+print(fromRoman("MIXX"))
