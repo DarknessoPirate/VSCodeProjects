f2531192ae833aedbd720fbe59a6ab48d9fed00b
DarknessoLAPTOP <darknesso.steam@tlen.pl>
2023-11-28 18:26:27 +0100

    zad37 added

---
+++ Python/Lista/Zad37.py
@@ -0,0 +1,15 @@
+
+import re
+import requests
+
+
+url = "https://platforma.polsl.pl/rms/"
+page = requests.get(url)
+html = page.text
+
+
+
+pattern = re.compile(r'^(href=")(.*")', re.IGNORECASE)
+a= re.findall('href="([^#].*?)"',html)
+for i in a:
+    print(i)
