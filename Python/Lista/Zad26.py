40f31672e54fc6d281a5b1a9f5552cadb0fbbaad
Dawid Pacia <dp307910@student.polsl.pl>
2023-11-28 01:36:37 +0100

    do dokonczenia 26

--- Python/Lista/Zad26.py
+++ Python/Lista/Zad26.py
@@ -1,4 +1,6 @@
 import math
+import cmath
+
 
 def factorial(n):
     if n==0:
@@ -13,3 +15,38 @@ def a(alpha, beta,op):
     else:
         print("Incorrect operator")
 
+print(f"factorial(5) = {factorial(5)}")
+
+def b(x,n):
+    p = 1
+    newN = n
+    f = 1
+    result = 1
+    for i in range(n):
+        result += (newN*pow(x,p))/factorial(f)
+        f += 1
+        p += 1
+        newN *= n - (i + 1)
+
+    return result
+
+print("b:",b(0.5,7))
+
+def c(a,b,c):
+    x1 = (-b - cmath.sqrt(b*b - 4*a*c))/2*a
+    x2 = (-b + cmath.sqrt(b*b - 4*a*c))/2*a
+    print(f"miejsca zerowe: {x1}, {x2}")
+
+c(3,4,5)
+
+def d(x):
+    result = 0
+    for i in range(150):
+        result += math.pow(x,i)/factorial(i)
+
+    return result
+x = 5
+print(f"e^{x} = {d(x)}")
+
+def e(a0,n,x):
+    result = a0
